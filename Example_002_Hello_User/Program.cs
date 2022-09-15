23ea987312883562adadaa9a9ea0ec01014f3504
Рабочая <yuo@exemple.com>
2022-09-15 20:55:07 +0700

    Example_002 Hello User

---
+++ Example_002_Hello_User/Program.cs
@@ -0,0 +1,8 @@
+﻿// Приложение 2. Приветствие пользователя
+Console.WriteLine("Ведите ваше имя:");
+string username = Console.ReadLine();
+Console.Write ("Привет, ");
+Console.Write (username);
+Console.Write ("!");
+Console.WriteLine (" ");
+Console.WriteLine ("Хорошего дня!");
