8f8483cc20a2a8566b6c2c58d0cc48f4074e7b43
Рабочая <yuo@exemple.com>
2022-09-16 00:00:13 +0700

    Вершины треугольника. Начало и ошибка

---
+++ Example_007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Приложение 7. Делим отрезки. Рисуем точки
+Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WritLine("*");
+
+int xa = 30, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WritLine("*");
+Console.SetCursorPosition(xb, yb);
+Console.WritLine("*");
+Console.SetCursorPosition(xc, yc);
+Console.WritLine("*");
