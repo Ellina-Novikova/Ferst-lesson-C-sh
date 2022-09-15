2cfcc84b7e5f91b4d84f15805c117a4d438136fd
Рабочая <yuo@exemple.com>
2022-09-15 23:25:12 +0700

    Example_005 if-else + ToLower()

---
+++ Example_005_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿// Приложение 5. Ветвление. Здороваемся с Машей
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
