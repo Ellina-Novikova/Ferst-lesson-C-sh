8581486e6b56e629e39258819895233672b709bf
Рабочая <yuo@exemple.com>
2022-09-15 22:35:08 +0700

    Example_003 Сложение двух чисел

---
+++ Example_003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Приложение 3. Сложение двух чисел
+int numberA = 8;
+int numberB = 12;
+int result = numberA + numberB;
+Console.Write(numberA);
+Console.Write("+");
+Console.Write(numberB);
+Console.Write("=");
+Console.WriteLine(result);
