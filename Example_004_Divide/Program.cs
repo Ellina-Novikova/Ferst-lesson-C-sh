26cc721e9d86bd8c7b5b9eea30863f790cd50bba
Рабочая <yuo@exemple.com>
2022-09-15 22:48:55 +0700

    Example_004L Деление двух чисел

---
+++ Example_004_Divide/Program.cs
@@ -0,0 +1,9 @@
+﻿// Приложение 4. Деление двух чисел
+double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB;
+Console.Write(numberA);
+Console.Write(":");
+Console.Write(numberB);
+Console.Write("=");
+Console.WriteLine(result);
