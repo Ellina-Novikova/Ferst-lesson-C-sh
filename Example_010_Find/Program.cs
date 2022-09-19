fcc5ff07bfaba9f9cf2b8a774b244c97dc676ee3
Рабочая <yuo@exemple.com>
2022-09-19 23:58:07 +0700

    find index

---
+++ Example_010_Find/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поискэлемента массива, равного Find
+//              0     1   2   3   4   5   6   7
+int[] array = {100, 122, 13, 42, 25, 64, 17, 83};
+int n = array.Length;
+int find = 122;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
