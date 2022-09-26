36a4bda4a9657f59f691fcbb0886cda681794687
Рабочая <yuo@exemple.com>
2022-09-26 20:06:38 +0700

    Методы, возвращающие значение

---
+++ Example_L3_02_return/Program.cs
@@ -0,0 +1,26 @@
+﻿// 3. Метод без входного аргумента, возвращающий значение
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+// 4. Метод, принимающий аргумент и возвращающий значение
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = "";  // string result = String.Empty;  обнуление строки
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "am");
+Console.WriteLine(res);
