659359fac0c7a18f16cf7d028b49ee582306b7ba
Рабочая <yuo@exemple.com>
2022-09-26 20:18:27 +0700

    Метод с циклом for

---
+++ Example_L3_03_For/Program.cs
@@ -0,0 +1,14 @@
+﻿// Метод4 с циклом FOR
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "am");
+Console.WriteLine(res);
